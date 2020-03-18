982b658f510413d4ae730c8390266f62f09c9fb1
Kevin Joseph <josephkevin1995@gmail.com>
2020-03-18 00:00:07 -0400

    profile hexagons added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,25 +10,6 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-
-    const iAmOptions = {
-      strings: ['I am a web developer.', 'I am a UI developer.'],
-      typeSpeed: 60,
-      smartBackspace: true,
-      showCursor: false
-    };
-
-    new Typed('.iam', iAmOptions);
-
-    const iCanOptions = {
-      strings: ['I can solve all your problems.', 'I can solve most of your problems.', 'I can solve problems.', 'I can create and deploy fast and effective solutions.'],
-      typeSpeed: 60,
-      smartBackspace: true,
-      showCursor: false,
-      startDelay: 5000
-    }
-    new Typed('.ican', iCanOptions);
-  }
+  ngOnInit(): void {}
 
 }
